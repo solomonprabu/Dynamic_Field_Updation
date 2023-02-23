e9b025f145b8d863935551a3054cb0011b7b42ac
solomonprabu <solomonprabu002@gmail.com>
2023-02-23 11:20:28 +0530

    css updation

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var counter = 1;
 function add_more_field(){
-    counter+=1
+    counter += 1
     html = '<div>\
     <label>Product name</label>\
     <input type="text" name="product'+counter+'" class="form-control">\
@@ -10,6 +10,6 @@ function add_more_field(){
     <input type="text" name="price'+counter+'" class="form-control">\
 </div>'
 
-var form = document.getElementById('form')
-form.innerHTML += html
+var form = document.getElementById('form');
+form.innerHTML += html;
 }
