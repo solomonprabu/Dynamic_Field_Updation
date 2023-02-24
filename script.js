349f4886bf32aa062a9b5e6e8b28632904a2dcb3
solomonprabu <solomonprabu002@gmail.com>
2023-02-24 12:54:50 +0530

    CSS

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ var counter = 1;
 function add_more_field(){
     counter += 1
     html = '<div>\
-    <label>Product name</label>\
+    <label>Product name</label><br>\
     <input type="text" name="product'+counter+'" class="form-control">\
 </div>\
     <div>\
-    <label>Price</label>\
+    <label>Price</label><br>\
     <input type="text" name="price'+counter+'" class="form-control">\
 </div>'
 
